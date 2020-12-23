776f18d00d482b456c1fc5df9c893b9f007d18dc
epicX <souravgain605@gmail.com>
2020-12-24 01:20:11 +0530

    modifed custom theme logic

--- gui/action.cpp
+++ gui/action.cpp
@@ -2907,9 +2907,7 @@ int GUIAction::applyCustomTheme(std::string arg){
 	GUIAction::themeInit("dummy");
 
 	//Applying custom theme
-	TM.applyCustomTheme();
-
-	err=TM.syncDyanmicVar() == true ? false : true;
+	err=TM.applyCustomTheme() == true ? false : true;
 
 	if(!err){
 		GUIAction::c_repack("dummy");
@@ -3052,16 +3050,12 @@ int GUIAction::fileManagerOp(std::string arg __unused){
 			result = false;
 		}
 		//Creating env for theme patching
-		if(result){
-			GUIAction::themeInit("dummy");
-		}
+		if(result) GUIAction::themeInit("dummy");
 
 		//Applying custom theme
 		if(result){
-			TM.applyCustomTheme();
+			result = TM.applyCustomTheme();
 		}
-
-		result = TM.syncDyanmicVar();
 
 		if(result){
 			GUIAction::c_repack("dummy");
