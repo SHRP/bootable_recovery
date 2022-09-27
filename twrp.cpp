85551533e61c18d42088799cb4c885b9b82c8d2b
rxuglr <rxuglr@mail.ru>
2022-09-28 00:57:21 +0300

    Small fix

--- twrp.cpp
+++ twrp.cpp
@@ -97,10 +97,10 @@ static void Decrypt_Page(bool SkipDecryption, bool datamedia) {
 				DataManager::SetValue("tw_crypto_user_id", "0");
 			if (gui_startPage("decrypt", 1, 1) != 0) {
 				LOGERR("Failed to start decrypt GUI page.\n");
-			} else {
 			}
 		}
 	} else if (datamedia) {
+		PartitionManager.Update_System_Details();
 		if (tw_get_default_metadata(DataManager::GetSettingsStoragePath().c_str()) != 0) {
 			LOGINFO("Failed to get default contexts and file mode for storage files.\n");
 		} else {
