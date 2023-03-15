TeamWin/android_bootable_recovery

decryption: solve specific decryption problems on some devices
Change-Id: I832c16da3d45888299fd9f96888ed8a85e5d48c9

Android.mk
@@ -183,6 +183,10 @@ ifeq ($(TW_NO_FLASH_CURRENT_TWRP),true)
     LOCAL_CFLAGS += -DTW_NO_FLASH_CURRENT_TWRP
 endif
 
+ifeq ($(TW_FIX_DECRYPTION_ON_DATA_MEDIA),true)
+    LOCAL_CFLAGS += -DTW_FIX_DECRYPTION_ON_DATA_MEDIA
+endif
+
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/bin
 
 ifeq ($(TARGET_RECOVERY_TWRP_LIB),)

partition.cpp
@@ -666,6 +666,10 @@ void TWPartition::Setup_Data_Partition(bool Display_Error) {
 	UnMount(false);
 
 #ifdef TW_INCLUDE_CRYPTO
+	#ifdef TW_FIX_DECRYPTION_ON_DATA_MEDIA
+	if (datamedia)
+		Setup_Data_Media();
+	#endif
 	Can_Be_Encrypted = true;
 	char crypto_blkdev[255];
 	property_get("ro.crypto.fs_crypto_blkdev", crypto_blkdev, "error");
@@ -673,8 +677,10 @@ void TWPartition::Setup_Data_Partition(bool Display_Error) {
 		Set_FBE_Status();
 		Decrypted_Block_Device = crypto_blkdev;
 		LOGINFO("Data already decrypted, new block device: '%s'\n", crypto_blkdev);
+		#ifndef TW_FIX_DECRYPTION_ON_DATA_MEDIA
 		if (datamedia)
 			Setup_Data_Media();
+		#endif
 		DataManager::SetValue(TW_IS_ENCRYPTED, 0);
 	} else if (!Mount(false)) {
 		if (Is_Present) {
@@ -716,9 +722,10 @@ void TWPartition::Setup_Data_Partition(bool Display_Error) {
 				LOGERR("Unable to decrypt FBE device\n");
 		} else {
 			DataManager::SetValue(TW_IS_ENCRYPTED, 0);
+			#ifndef TW_FIX_DECRYPTION_ON_DATA_MEDIA
 			if (datamedia)
 				Setup_Data_Media();
-
+			#endif
 		}
 	}
 	if (datamedia && (!Is_Encrypted || (Is_Encrypted && Is_Decrypted))) {
@@ -1232,7 +1239,11 @@ void TWPartition::Setup_Data_Media() {
 			Symlink_Mount_Point = "/sdcard";
 		}
 		Mount(false);
+		#ifdef TW_FIX_DECRYPTION_ON_DATA_MEDIA
+		if (Mount(false) && TWFunc::Path_Exists(Mount_Point + "/media/0")) {
+		#else
 		if (TWFunc::Path_Exists(Mount_Point + "/media/0")) {
+		#endif
 			Storage_Path = Mount_Point + "/media/0";
 			Symlink_Path = Storage_Path;
 			DataManager::SetValue(TW_INTERNAL_PATH, Mount_Point + "/media/0");

SHRP/bootable_recovery

Android.mk
@@ -207,6 +207,10 @@ ifeq ($(TW_NO_FLASH_CURRENT_TWRP),true)
     LOCAL_CFLAGS += -DTW_NO_FLASH_CURRENT_TWRP
 endif
 
+ifeq ($(TW_PREPARE_DATA_MEDIA_EARLY),true)
+    LOCAL_CFLAGS += -DTW_PREPARE_DATA_MEDIA_EARLY
+endif
+
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/bin
 
 ifeq ($(TARGET_RECOVERY_TWRP_LIB),)

partition.cpp
@@ -666,6 +666,10 @@ void TWPartition::Setup_Data_Partition(bool Display_Error) {
 	UnMount(false);
 
 #ifdef TW_INCLUDE_CRYPTO
+	#ifdef TW_PREPARE_DATA_MEDIA_EARLY
+	if (datamedia)
+		Setup_Data_Media();
+	#endif
 	Can_Be_Encrypted = true;
 	char crypto_blkdev[255];
 	property_get("ro.crypto.fs_crypto_blkdev", crypto_blkdev, "error");
@@ -673,8 +677,10 @@ void TWPartition::Setup_Data_Partition(bool Display_Error) {
 		Set_FBE_Status();
 		Decrypted_Block_Device = crypto_blkdev;
 		LOGINFO("Data already decrypted, new block device: '%s'\n", crypto_blkdev);
+		#ifndef TW_PREPARE_DATA_MEDIA_EARLY
 		if (datamedia)
 			Setup_Data_Media();
+		#endif
 		DataManager::SetValue(TW_IS_ENCRYPTED, 0);
 	} else if (!Mount(false)) {
 		if (Is_Present) {
@@ -716,9 +722,10 @@ void TWPartition::Setup_Data_Partition(bool Display_Error) {
 				LOGERR("Unable to decrypt FBE device\n");
 		} else {
 			DataManager::SetValue(TW_IS_ENCRYPTED, 0);
+			#ifndef TW_PREPARE_DATA_MEDIA_EARLY
 			if (datamedia)
 				Setup_Data_Media();
-
+			#endif
 		}
 	}
 	if (datamedia && (!Is_Encrypted || (Is_Encrypted && Is_Decrypted))) {
@@ -1231,8 +1238,12 @@ void TWPartition::Setup_Data_Media() {
 			Make_Dir("/sdcard", false);
 			Symlink_Mount_Point = "/sdcard";
 		}
+		#ifdef TW_PREPARE_DATA_MEDIA_EARLY
+		if (Mount(false) && TWFunc::Path_Exists(Mount_Point + "/media/0")) {
+		#else
 		Mount(false);
 		if (TWFunc::Path_Exists(Mount_Point + "/media/0")) {
+		#endif
 			Storage_Path = Mount_Point + "/media/0";
 			Symlink_Path = Storage_Path;
 			DataManager::SetValue(TW_INTERNAL_PATH, Mount_Point + "/media/0");